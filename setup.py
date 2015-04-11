62da23697be428b088aa0a137f4e2e1ccc6a1e71
Matt Hooks <me@matthooks.com>
2015-04-11 19:31:55 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='aiohttp',
+    version='0.1',
+    packages=['aiotest'],
+    url='',
+    license='MIT',
+    author='Matt Hooks',
+    author_email='me@matthooks.com',
+    description=''
+)
