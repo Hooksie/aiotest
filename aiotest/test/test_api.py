b01ce1c40d9444fb50bfc0e0e9a732d1c1117aa7
Matt Hooks <me@matthooks.com>
2015-04-12 13:18:49 -0400

    Rename test cases to match TestCase API, add tests for duration

--- aiotest/test/test_api.py
+++ aiotest/test/test_api.py
@@ -9,17 +9,17 @@ def simple_add(a, b):
 
 class BasicApiTests(TestCase):
 
-    def test_simple_equal_result(self):
+    def test_assert_coro_result(self):
         self.assertCoroResult(4, simple_add, 2, 2)
 
-    def test_simple_equal_result_incorrect(self):
+    def test_assert_coro_result_incorrect(self):
         with self.assertRaises(TestCase.failureException):
             self.assertCoroResult(20, simple_add, 2, 2)
 
-    def test_simple_unequal_result(self):
+    def test_assert_coro_not_result(self):
         self.assertCoroNotResult(20, simple_add, 2, 2)
 
-    def test_simple_unequal_result_incorrect(self):
+    def test_assert_coro_not_result_incorrect(self):
         with self.assertRaises(TestCase.failureException):
             self.assertCoroNotResult(4, simple_add, 2, 2)
 
@@ -29,3 +29,20 @@ class BasicApiTests(TestCase):
             self.assertCoroResult(4, simple_add, 2, 2, msg=msg)
         except TestCase.failureException as err:
             self.assertEqual(msg, str(err))
+
+    def test_assert_coro_duration(self):
+
+        @asyncio.coroutine
+        def takes_5_seconds():
+            yield from asyncio.sleep(5)
+
+        self.assertCoroDuration(5, takes_5_seconds())
+
+    def test_assert_coro_duration_incorrect(self):
+
+        @asyncio.coroutine
+        def takes_10_seconds():
+            yield from asyncio.sleep(10)
+
+        with self.assertRaises(self.failureException):
+            self.assertCoroDuration(5, takes_10_seconds())
