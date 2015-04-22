04eab3c5158f20fb4b4a8192f06d39e050fb2987
Harry Bock <bock.harryw@gmail.com>
2015-04-21 21:48:58 -0400

    aiotest.loop: break out TestableHandle tests

--- aiotest/test/test_loop.py
+++ aiotest/test/test_loop.py
@@ -3,25 +3,36 @@ import asyncio
 
 from aiotest import loop
 
-class TimeTravelingTestLoopTest(unittest.TestCase):
-    def setUp(self):
-        self.f = asyncio.Future()
-        self.event_loop = loop.TimeTravelingTestLoop()
-
+class TestableHandleTest(unittest.TestCase):
+    """
+    Test the TestableHandle implementation.
+    """
     def test_task_init(self):
+        """
+        Test initializing the internal data of the TestableHandle,
+        including capturing arguments.
+        """
         t = loop.TestableHandle(1.5, print, [1,2,3])
         self.assertEqual(1.5, t.delay)
         self.assertEqual(print, t.callback)
         self.assertEqual([1, 2, 3], t.args)
 
     def test_task_order_lt(self):
+        """
+        Ensure __lt__ works as expected.
+        """
         t1 = loop.TestableHandle(1.5, print, [1, 2, 3])
         t2 = loop.TestableHandle(2.0, print, [1, 2, 3])
 
         self.assertTrue(t1 < t2)
         self.assertFalse(t2 < t1)
 
-    def test_schedule_dont_run(self):
+class TimeTravelingTestLoopTest(unittest.TestCase):
+    def setUp(self):
+        self.f = asyncio.Future()
+        self.event_loop = loop.TimeTravelingTestLoop()
+
+    def test_call_later_dont_run(self):
         """
         A callback scheduled via call_later should not execute if
         time has not advanced.
@@ -29,7 +40,7 @@ class TimeTravelingTestLoopTest(unittest.TestCase):
         self.event_loop.call_later(5.0, self.f.set_result, 5643)
         self.assertFalse(self.f.done(), "Callback should not have been executed.")
 
-    def test_schedule_dont_run_after_advance(self):
+    def test_call_later_dont_run_after_advance(self):
         """
         A callback scheduled via call_later should not execute if
         the minimum amount of time has not yet advanced.
@@ -38,7 +49,7 @@ class TimeTravelingTestLoopTest(unittest.TestCase):
         self.event_loop.advance(4.0)
         self.assertFalse(self.f.done(), "Callback should not have been executed.")
 
-    def test_schedule_run_after_advance(self):
+    def test_call_later_run_after_advance(self):
         """
         A callback scheduled via call_later should execute if
         the minimum amount of time has advanced.
@@ -48,7 +59,7 @@ class TimeTravelingTestLoopTest(unittest.TestCase):
         self.assertTrue(self.f.done(), "Callback should have been executed.")
         self.assertEqual(5643, self.f.result())
 
-    def test_schedule_two_run_after_advance(self):
+    def test_call_later_two_run_after_advance(self):
         """
         If multiple callbacks are scheduled out-of-order, only the callback
         whose time as expired should execute.
@@ -62,7 +73,7 @@ class TimeTravelingTestLoopTest(unittest.TestCase):
         self.assertTrue(g.done(), "Second callback should have been executed.")
         self.assertEqual(1234, g.result())
 
-    def test_schedule_advance_run_in_order(self):
+    def test_call_later_advance_run_in_order(self):
         """
         If multiple callbacks are scheduled out-of-order, and multiple
         would have been called in the time advance()'d, they should
@@ -76,5 +87,3 @@ class TimeTravelingTestLoopTest(unittest.TestCase):
         self.event_loop.advance(6.0)
 
         self.assertEqual([1234, 2057, 5643], exec_order_list)
-
-    def tes
